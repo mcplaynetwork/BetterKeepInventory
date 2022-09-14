32f17c3aeb27f910e50ef9da5ba6bfa0f060a52f
snake <admin@viasnake.com>
2022-09-14 16:24:03 +0900

    BetterKeepInventory

--- src/main/java/biz/mcplay/betterkeepinventory/BetterKeepInventory.java
+++ src/main/java/biz/mcplay/betterkeepinventory/BetterKeepInventory.java
@@ -1,8 +1,7 @@
 package biz.mcplay.betterkeepinventory;
-
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class BetterKeepinventory extends JavaPlugin {
+public final class BetterKeepInventory extends JavaPlugin {
 
     @Override
     public void onEnable() {
